e8503f153edfc280cfcae463886fc2a572d42ea8
NeyJealous <rismahanov@yandex.ru>
2026-09-20 02:19:12 +0300

    Use generated VOT protocol constants

---
+++ VOT/VOTConfig.m
@@ -0,0 +1,10 @@
+#import "VOTConfig.h"
+#import "VOTGeneratedConfig.h"
+
+NSString * const VOTAPIHost = VOT_GENERATED_HOST;
+NSString * const VOTHMACKey = VOT_GENERATED_HMAC;
+NSString * const VOTUserAgent = VOT_GENERATED_USER_AGENT;
+NSString * const VOTComponentVersion = VOT_GENERATED_COMPONENT_VERSION;
+NSString * const VOTDefaultSourceLanguage = @"en";
+NSString * const VOTDefaultTargetLanguage = @"ru";
+NSString * const VOTStateChangedNotification = @"YTFreePlusVOTStateChanged";
