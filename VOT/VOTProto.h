72b2d5b29546e60f50070864a712f67b753e7258
NeyJealous <rismahanov@yandex.ru>
2026-09-20 02:19:57 +0300

    Add VOT protobuf interface

---
+++ VOT/VOTProto.h
@@ -0,0 +1,20 @@
+#import <Foundation/Foundation.h>
+@class VOTSession;
+@class VOTTranslation;
+
+NS_ASSUME_NONNULL_BEGIN
+@interface VOTProto : NSObject
++ (NSData *)sessionRequestWithUUID:(NSString *)uuid module:(NSString *)module;
++ (nullable VOTSession *)decodeSession:(NSData *)data;
++ (NSData *)translationRequestWithURL:(NSString *)url
+                            duration:(NSTimeInterval)duration
+                      sourceLanguage:(NSString *)sourceLanguage
+                      targetLanguage:(NSString *)targetLanguage
+                        firstRequest:(BOOL)firstRequest
+                         livelyVoice:(BOOL)livelyVoice;
++ (nullable VOTTranslation *)decodeTranslation:(NSData *)data;
++ (NSData *)emptyAudioRequestWithURL:(NSString *)url
+                       translationID:(NSString *)translationID
+                              fileID:(NSString *)fileID;
+@end
+NS_ASSUME_NONNULL_END
