6edcdc46c427dd1dda99fd6f83b1f190d5af8820
NeyJealous <rismahanov@yandex.ru>
2026-09-20 02:18:28 +0300

    Add VOT configuration interface

---
+++ VOT/VOTConfig.h
@@ -0,0 +1,13 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+FOUNDATION_EXPORT NSString * const VOTAPIHost;
+FOUNDATION_EXPORT NSString * const VOTHMACKey;
+FOUNDATION_EXPORT NSString * const VOTUserAgent;
+FOUNDATION_EXPORT NSString * const VOTComponentVersion;
+FOUNDATION_EXPORT NSString * const VOTDefaultSourceLanguage;
+FOUNDATION_EXPORT NSString * const VOTDefaultTargetLanguage;
+FOUNDATION_EXPORT NSString * const VOTStateChangedNotification;
+
+NS_ASSUME_NONNULL_END
